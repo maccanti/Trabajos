c7f108f3f57dad303e82ccf80ae6f763d9d90b0e
maccanti <140736060+maccanti@users.noreply.github.com>
2023-08-06 18:37:05 -0500

    Delete Trabajos/Ejercicios/Ejercicios 2 directory

--- Trabajos/Ejercicios/Ejercicios 2/2.py
+++
@@ -1,7 +0,0 @@
-class Punto():
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-
-mi_punto= Punto("4x","-7y")
-print("El punto se encuntra con las coordenadas", mi_punto.x, "y", mi_punto.y)
