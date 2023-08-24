db6587211f3e2672cba3ecaeab997176600c7044
maccanti <140736060+maccanti@users.noreply.github.com>
2023-08-23 22:37:28 -0500

    Add files via upload

---
+++ fafafa/Lista.py
@@ -0,0 +1,11 @@
+lista = list["Hola",5,True,7,8.8]
+#              0   1   2  3  4
+#             -5   -4  -3 -2 -1
+print(lista[0:2])
+print(lista[:4])
+print(lista[3:])
+print(lista[-5:-2])
+print(lista[0:2]+lista[0:4])
+print("Hola" not in lista)
+print(len(lista))
+ 
